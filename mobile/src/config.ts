f84f743669f329d30852c2add836a896aa2099b1
Jacob3008 <jsneuman2@gmail.com>
2026-03-17 16:31:44 -0400

    Fix ios build errors

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -2,7 +2,7 @@ import {Platform} from 'react-native';
 
 const LOCAL_API_BASE_URL = Platform.select({
   android: 'http://10.0.2.2:3000',
-  ios: 'http://localhost:3000',
+  ios: 'http://192.168.4.71:3000',
   default: 'http://localhost:3000',
 });
 
